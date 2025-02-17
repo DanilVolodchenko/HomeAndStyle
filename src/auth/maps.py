77d95477e441a72ff93ac74bbde2bfae57b58664
Danil Volodchenko <danil.volodchenko@inbox.ru>
2025-02-17 21:23:42 +0700

    Adding id to map

--- src/auth/maps.py
+++ src/auth/maps.py
@@ -4,6 +4,7 @@ from .models.dto import UserDto
 
 def map__user_model__user_dto(user_model: UserModel) -> UserDto:
     return UserDto(
+        id=user_model.id,
         email=user_model.email,
         role=user_model.role,
         send_email=user_model.send_email
